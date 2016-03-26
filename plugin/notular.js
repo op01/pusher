1e8bfa346d4d528bbad97b100159459af5a89e37
OOP <opkampitak@hotmail.com>
2016-03-26 23:42:23 +0700

    Second commit

---
+++ plugin/notular.js
@@ -0,0 +1,19 @@
+/*global angular,io*/
+var notular=angular.module("notular",["toaster"]).run(function(socket,notification,toaster){
+    notification.requestPermission();
+    socket.on("push",function(data){
+        new notification("new notification!",{"body":data.message});
+        toaster.pop({body: data.message, title:"new notification!",timeout:-1});
+    });
+});
+// http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
+notular.factory('notification',function($window){
+    return $window.Notification;
+});
+notular.factory('socket', function ($rootScope) {
+    var socket = io('wss://pusher.azurewebsites.net');
+    return {
+        on:function(eventName,callback){socket.on(eventName,function(){var args=arguments;$rootScope.$apply(function(){callback.apply(socket, args);});});},
+        emit:function(eventName,data,callback){socket.emit(eventName,data,function(){var args=arguments;$rootScope.$apply(function(){if(callback){callback.apply(socket,args);}});})}
+    };
+});
